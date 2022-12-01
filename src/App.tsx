7f97482bbd79a6d62e23da3a0b878c4924d30a6e
kalz2q <katsu95i@gmail.com>
2022-12-01 14:47:45 +0900

    M

--- src/App.tsx
+++ src/App.tsx
@@ -4,50 +4,23 @@ const { useState } = React;
 
 function App() {
   // const [inputValue, setInputValue] = useState("おｋ");
-  const [todos, setTodos] = useState<Todo[]>(ITEMS);
+  // const [todos, setTodos] = useState<Todo[]>(ITEMS);
+  const todos = ITEMS;
 
   type Todo = {
     id: number,
     title: string,
     body: string
   }
-
-  // const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-  // const handleChange = (e: any) => {
-  //   setInputValue(e.target.value);
-  // };
-
-  // const handleSubmit = (e: any) => {
-  //   e.preventDefault();
-  //   if (inputValue !== "") {
-  //     const newTodo: Todo = {
-  //       inputValue: inputValue,
-  //       id: todos.length
-  //     };
-  //     // setTodos([newTodo, ...todos]);
-  //     setTodos(todos.concat(newTodo));
-  //     setInputValue("");
-  //     console.log(todos);
-  //   }
-  // }
-
   return (
     <div className="App">
       <div>
         <h2>todo with map</h2>
-        {/* 
-        <form onSubmit={(e) => handleSubmit(e)}>
-          <input type="text"
-            autoFocus={true}
-            value={inputValue}
-            onChange={(e) => handleChange(e)}
-            className="inputText" />
-          <input type="submit" value="submit" className="submitButton" />
-        </form> */}
         <ul className="todoList">
           {todos.map((todo) => (
             <li key={todo.id}>
               <p>{todo.title}</p>
+              <p>{todo.body}</p>
             </li>
           ))}
         </ul>
@@ -61,17 +34,17 @@ const ITEMS = [
   {
     id: 1,
     title: "一番最初のタイトルが入ります",
-    body: "テキストが入りますテキストが入りますテキストが入ります"
+    body: "おっとテキストが入りますテキストが入りますテキストが入ります"
   },
   {
     id: 2,
     title: "2番目のタイトルが入ります",
-    body: "テキストが入りますテキストが入りますテキストが入ります"
+    body: "テキストが入テ".concat("\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0キストが 入りますテキスト").concat("\xa0".repeat(20)).concat("が入ります")
   },
   {
     id: 3,
     title: "3番目のタイトルが入ります",
-    body: "テキストが入りますテキストが入りますテキストが入ります"
+    body: "テキストが入ります\xa0\xa0\xa0\xa0テキストが入りますテキストが入ります"
   },
 ]
 
